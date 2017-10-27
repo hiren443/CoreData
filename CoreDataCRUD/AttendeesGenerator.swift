3f19308bfd22bb9199a49b07308bd00250829f1e
Hiren Bhadreshwara <hiren@indusfacetelecom.com>
2017-10-27 12:20:00 +0530

    Initial commit
    Source
    https://github.com/jongensvantechniek/CoreData-CRUD-Swift-3.1-example/bl
    ob/master/README.md

---
+++ CoreDataCRUD/AttendeesGenerator.swift
@@ -0,0 +1,52 @@
+//
+//  AttendeesGenerator.swift
+//  CoreDataCRUD
+//
+//  Copyright © 2016 Jongens van Techniek. All rights reserved.
+//
+
+import Foundation
+
+class AttendeesGenerator {
+
+    /**
+    Generate (pseudo) random attendeeslist of (pseudo) random size and (pseudo) random attendees.
+    
+    For true randomness see:
+    - Link: https://en.wikipedia.org/wiki/Hardware_random_number_generator
+    
+    - Returns: Array<String> A (pseudo) random generated attendeeslist
+    */
+    class func getSemiRandomGeneratedAttendeesList() -> Array<String> {
+        let optionalAttendees = [
+            "Yoda",
+            "HAL 9000",
+            "Gizmo",
+            "Optimus Prime",
+            "Marvin the Paranoid Android",
+            "ET",
+            "Bender",
+            "Narcissus",
+            "Frodo",
+            "Esscher",
+            "Lothar Collatz",
+            "Foo",
+            "Bar",
+            "Tweety"
+        ]
+
+        var attendeesList = [String]()
+        let listSize: Int =  Int(arc4random_uniform(UInt32(truncatingBitPattern: optionalAttendees.count)))
+
+        for _ in 0...listSize {
+            let itemIndex: Int =  Int(arc4random_uniform(UInt32(truncatingBitPattern: listSize)))
+            let item: String = optionalAttendees[itemIndex]
+            if !attendeesList.contains(item) {
+                attendeesList.append(item)
+            }
+        }
+
+        return attendeesList
+    }
+
+}
